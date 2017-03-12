5965b2a3c91f38691fb85e8a610f40757ab39286
Eduardo de Vera Toquero <eduardo.devera@gmail.com>
2017-03-12 23:38:25 +0100

    Improvements in parameter handling

--- annotation-processor/src/main/java/org/devera/jest/processor/ReSTOperationAnnotatedMethod.java
+++ annotation-processor/src/main/java/org/devera/jest/processor/ReSTOperationAnnotatedMethod.java
@@ -1,6 +1,8 @@
 package org.devera.jest.processor;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 import javax.lang.model.element.ExecutableElement;
@@ -9,7 +11,9 @@ import javax.lang.model.type.TypeMirror;
 
 import org.devera.jest.annotations.ReSTHeaderParam;
 import org.devera.jest.annotations.ReSTOperation;
+import org.devera.jest.annotations.ReSTPathParam;
 import org.devera.jest.annotations.ReSTQueryParam;
+import org.devera.jest.client.params.BodyParam;
 import org.devera.jest.client.params.HeaderParam;
 import org.devera.jest.client.params.NamedParam;
 import org.devera.jest.client.params.PathParam;
@@ -29,14 +33,27 @@ class ReSTOperationAnnotatedMethod {
         return this.operationElement.getSimpleName().toString();
     }
 
-    List<Parameter> getArgumentMapNameAndType() {
-        return operationElement.getParameters().stream()
+    List<Parameter> getArgumentMapNameAndType(Predicate<VariableElement> filter) {
+        return operationElement.getParameters()
+                .stream()
+                .filter(filter)
                 .map(
                         typeParameter -> new Parameter(
                                 typeParameter.getSimpleName().toString(),
                                 typeParameter.asType(),
                                 getType(typeParameter))
                 ).collect(Collectors.toList());
+    }
+
+    public static Predicate<VariableElement> isParameter() {
+        return param -> hasAnyAnnotation(param, ReSTQueryParam.class, ReSTPathParam.class, ReSTHeaderParam.class);
+    }
+
+    private static boolean hasAnyAnnotation(VariableElement param, Class... annotations) {
+        for (Class annotation : annotations) {
+            if (param.getAnnotation(annotation) != null) return true;
+        }
+        return false;
     }
 
     ReSTOperation getAnnotation() {
@@ -50,7 +67,27 @@ class ReSTOperationAnnotatedMethod {
     private static Parameter.Type getType(final VariableElement typeParameter) {
         if (typeParameter.getAnnotation(ReSTQueryParam.class) != null) return Parameter.Type.QUERY;
         if (typeParameter.getAnnotation(ReSTHeaderParam.class) != null) return Parameter.Type.HEADER;
-        return Parameter.Type.PATH;
+        if (typeParameter.getAnnotation(ReSTPathParam.class) != null) return Parameter.Type.PATH;
+        return Parameter.Type.BODY;
+    }
+
+    Optional<Parameter> getRequestArgument() {
+        for (VariableElement parameter : this.operationElement.getParameters()) {
+            if (!hasAnyAnnotation(parameter, ReSTQueryParam.class, ReSTHeaderParam.class, ReSTQueryParam.class)) {
+                return Optional.of(
+                        new Parameter(
+                                parameter.getSimpleName().toString(),
+                                parameter.asType(),
+                                getType(parameter)
+                        )
+                );
+            }
+        }
+        return Optional.empty();
+    }
+
+    public static Predicate<VariableElement> all() {
+        return param -> true;
     }
 
     static class Parameter {
@@ -64,7 +101,7 @@ class ReSTOperationAnnotatedMethod {
                 case QUERY:
                     return QueryParam.class;
                 default:
-                    throw new IllegalStateException();
+                    return BodyParam.class;
             }
         }
 
@@ -75,7 +112,8 @@ class ReSTOperationAnnotatedMethod {
         public enum Type {
             QUERY,
             PATH,
-            HEADER
+            HEADER,
+            BODY
         }
 
         private final TypeMirror typeMirror;
